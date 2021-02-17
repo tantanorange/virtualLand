9dce40f2557fabe358a8a7ca1569ae7dd785568b
Kun <44329336+tantanorange@users.noreply.github.com>
2021-02-17 18:25:01 -0500

    New test

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import string
+
+# response = requests.get("https://api.decentraland.org/v2/tiles?x1={}&y1={}&x2={}&y2={}".format(10,10,15,15))
+response = requests.get('https://api.decentraland.org/v2/parcels/{}/{}'.format(-118, -118))
+
+print(response.status_code)
+# response -> json
+print(response.json())
+print('----------------------------------------------------------------------------------')
+print('----------------------------------------------------------------------------------')
+properties = json.loads(response.text)
+print(properties)
